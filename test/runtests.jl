022fe83e5726ebb9e5de536c5a0bf3ebebddd733
tsaid2 <tuami.fatu@gmail.com>
2019-07-11 15:23:17 +0200

    new Reverse String

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,11 +7,11 @@ for tests in [
             #"runtestAdd.jl", 			# pb
 			#"runtestLogicalOr.jl",		#
 			#"runtestLogicalXor.jl", 	#
-			#"runtestReverseString.jl",	# pb, don't know why it doesn't work, working on
-			"runtestLengthString.jl"	#
-			#"runtestString.jl",		#
-			#"runtestLogicalAnd.jl",		#
-			#"runtestRepeat.jl",		#
+			"runtestReverseString.jl",	# wroked once, pb, don't know why it doesn't work, working on
+			#"runtestLengthString.jl"	# work twice and didn't try since
+			#"runtestString.jl",		# work sure
+			#"runtestLogicalAnd.jl",	# wrok with extended sure
+			#"runtestRepeat.jl",		# work sure
 ]
     include(tests)
 end
